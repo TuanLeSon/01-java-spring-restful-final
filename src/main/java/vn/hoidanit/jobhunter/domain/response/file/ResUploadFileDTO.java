0653791859b27621d56a358f9ecb7aa27255739a
TuanLeSon <78398071+TuanLeSon@users.noreply.github.com>
2024-09-29 21:16:43 +0700

    #108

--- src/main/java/vn/hoidanit/jobhunter/domain/response/file/ResUploadFileDTO.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/response/file/ResUploadFileDTO.java
@@ -12,6 +12,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResUploadFileDTO {
-    private String name;
+    private String fileName;
     private Instant uploadedAt;
 }
